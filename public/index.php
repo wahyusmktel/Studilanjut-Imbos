8c286125d347a85bbb28a16679ff629f9b6cc0df
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2026-08-02 09:26:57 +0700

    Refactor index.php for improved readability and structure

--- public/index.php
+++ public/index.php
@@ -4,19 +4,21 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// 1. Cek mode maintenance
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../laravel/vendor/autoload.php';
+// 2. Load Composer Autoloader (INI WAJIB ADA SEBELUM BOOTSTRAP)
+require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../laravel/bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// 3. Bootstrap Laravel dan simpan ke variabel $app
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-//set the public to this directory
-$app->bind('path.public', function() {
-return __DIR__ ;
+// 4. Set public path
+$app->bind('path.public', function () {
+    return __DIR__;
 });
+
+// 5. Handle request
+$app->handleRequest(Request::capture());
